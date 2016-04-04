4152c41c64e355b38b324a8267af8412a4813ade
aprudeaux <aprudeaux@yahoo.com>
2016-04-04 04:36:04 +0000

    Add seller page

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   get 'property_locator' => 'pages#property_locator'
   
   get 'realtors' => 'pages#realtors'
+  
+  get 'seller' => "listings#seller"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
